7c990f665200ec35435209a6f5f9938dbb02ca51
Allaanm <allanmuli18@gmail.com>
2026-01-06 16:34:44 +0300

    new commit

--- Backend/FaviconGen/FaviconGen/wsgi.py
+++ Backend/FaviconGen/FaviconGen/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Add the project root directory to the Python path
+# This allows 'FaviconGen.settings' to be imported correctly
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
